972e5b50e1e05ee4b4563e67a2d7e471705bad65
SkelletonX <kingofmustache@gmail.com>
2021-11-14 16:37:43 -0300

    Create Reset all Server.sql

---
+++ Db/Query/Reset all Server.sql
@@ -0,0 +1,59 @@
+use Db_Tank41
+/* Dados usuarios */
+delete from Sys_Eat_Pets;
+delete from Sys_User_Gemstone;
+delete from Sys_User_Info;
+delete from Sys_User_Match_Info;
+delete from Sys_User_Rank;
+delete from Sys_Users_Card;
+delete from Sys_Users_Detail;
+delete from Sys_Users_EventProcess;
+delete from Sys_Users_Extra;
+delete from Sys_Users_Fight;
+delete from Sys_Users_Friends;
+delete from Sys_Users_Gift;
+delete from Sys_Users_Goods;
+delete from Sys_Users_Labyrinth;
+delete from Sys_Users_Log;
+delete from Sys_Users_Order;
+delete from Sys_Users_Password;
+delete from Sys_Users_Pet;
+delete from Sys_Users_Record;
+delete from Sys_Users_Texp;
+delete from Sys_VIP_Info;
+delete from User_Messages;
+delete from User_Buff;
+
+/* Dados do Servidor */
+delete from Rename_Nick
+delete from Rename_Consortia
+delete from QuestData
+delete from DailyRecordInfo
+delete from Charge_Money /* Sistema recarga */
+delete from AchievementData
+delete from Active_Number
+
+/* Dados Sociedade */
+delete from Consortia
+delete from Consortia_Ally
+delete from Consortia_Apply_Ally
+delete from Consortia_Apply_Users
+delete from Consortia_Event
+delete from Consortia_Invite_Users
+delete from Consortia_Level
+delete from Consortia_Users
+
+-- DROP TABELA VIP_INFO2
+drop table Sys_VIP_Info2;
+
+--------------------------------- Db_Membership ---------------------------------
+
+use Db_Membership
+
+delete from Mem_UserInfo;
+delete from Mem_Users;
+delete from Webshop_Account;
+delete from Webshop_Bag;
+delete from Mem_UsersInRoles;
+
+
